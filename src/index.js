11e19026eede1798e7782ce1bf42da520befa9a1
momotvit <110504519+momotvit@users.noreply.github.com>
2023-03-19 23:39:07 -0700

    ready to be checked

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ const countryListEl = document.querySelector('.country-list');
 
 searchEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
+
+/////////// on input changes in markup
+
+
 function onInput(e) {
     const countryName = (e.target.value).trim();
     if (!countryName) {
@@ -24,11 +28,39 @@ function onInput(e) {
         .then(countries => {
             clearMarkup();
                if (countries.length > 10) {
-            return Notify.info("Too many matches found. Please enter a more specific name.");
+            return Notify.info("Too many matches found. Please enter a more specific name.",{
+                className: 'notiflix-report',
+                width: '320px',
+                backgroundColor: '#f8f8f8',
+                borderRadius: '25px',
+                rtl: false,
+                zindex: 4002,
+                backOverlay: true,
+                backOverlayColor: 'rgba(0,0,0,0.5)',
+                backOverlayClickToClose: false,
+                fontFamily: 'Quicksand',
+                svgSize: '110px',
+                plainText: true,
+                titleFontSize: '16px',
+                titleMaxLength: 34,
+                messageFontSize: '13px',
+                messageMaxLength: 400,
+                buttonFontSize: '14px',
+                buttonMaxLength: 34,
+                cssAnimation: true,
+                cssAnimationDuration: 360,
+                cssAnimationStyle: 'fade' - 'zoom',
+                svgColor: '#ff5549',
+                titleColor: '#1e1e1e',
+                messageColor: '#242424',
+                buttonBackground: '#ff5549',
+                buttonColor: '#fff',
+                backOverlayColor: 'rgba(255,85,73,0.2)',
+        });
         } else if (countries.length >= 2 && countries.length <= 10) {
             return countries.map(renderCountryMarkup);
         } else if (countries.length === 1) {
-            return renderMarkupDiv(countries);
+            return renderCountryInfo(countries);
         }
         })
        .catch((error) => {
@@ -36,9 +68,11 @@ function onInput(e) {
         clearMarkup();
      })
 }
+ 
 
+/////markup for country info
 
-function renderMarkupDiv(countries) {
+function renderCountryInfo(countries) {
     return countries.map((country) => {
         const { name, capital, population, flags, languages } = country;
         const listOfLangs = Object.values(languages).join(', ');
@@ -47,7 +81,7 @@ function renderMarkupDiv(countries) {
             `<div class="header-wrapper">
                 <img src="${flags.svg}" alt="${name.official}" width="40" height="27">
                 <h1>${name.official}</h1>
-                </div>
+            </div>
                 <p><span class="accent">Capital:</span> ${capital}</p>
                 <p><span class="accent">Population:</span> ${population}</p>
                 <p><span class="accent">Languages:</span> ${listOfLangs}</p>`
@@ -55,6 +89,9 @@ function renderMarkupDiv(countries) {
     })
 };
  
+
+///////////markup list of contries
+
 function renderCountryMarkup(country) {
     const { 
             name,  
@@ -65,13 +102,48 @@ function renderCountryMarkup(country) {
     
 }
 
+
+/////////deleting markups
+
+
 function clearMarkup() {
     countryEl.innerHTML = '';
     countryListEl.innerHTML = '';
 }
 
 
-function showWarning(error) { Notify.failure('Oops, there is no country with that name'); };
+/////////showing warning messages using notifyMessage
+
+function showWarning(error) { Notify.failure('Oops, there is no country with that name',{
+                className: 'notiflix-report',
+                width: '320px',
+                backgroundColor: '#f8f8f8',
+                borderRadius: '25px',
+                rtl: false,
+                zindex: 4002,
+                backOverlay: true,
+                backOverlayColor: 'rgba(0,0,0,0.5)',
+                backOverlayClickToClose: false,
+                fontFamily: 'Quicksand',
+                svgSize: '110px',
+                plainText: true,
+                titleFontSize: '16px',
+                titleMaxLength: 34,
+                messageFontSize: '13px',
+                messageMaxLength: 400,
+                buttonFontSize: '14px',
+                buttonMaxLength: 34,
+                cssAnimation: true,
+                cssAnimationDuration: 360,
+                cssAnimationStyle: 'fade' - 'zoom',
+                svgColor: '#ff5549',
+                titleColor: '#1e1e1e',
+                messageColor: '#242424',
+                buttonBackground: '#ff5549',
+                buttonColor: '#fff',
+                backOverlayColor: 'rgba(255,85,73,0.2)',
+})
+};
 
 
 
